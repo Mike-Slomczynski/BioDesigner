1ae463a9639a74176d6248f65984c927beecc4f6
mike-slomczynski-redmane <mike.slomczynski@gmail.com>
2023-04-21 14:12:59 -0500

    Added Simulation Result Model.

---
+++ BioDesigner/Client/Models/SimulationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace BioDesigner.Client.Models
+{
+    public class SimulationResult
+    {
+        public string? Id { get; set; }
+        public GeneticCircuit? Circuit { get; set; }
+        public DateTime Timestamp { get; set; }
+        public double Score { get; set; }
+        public IDictionary<string, object> AdditionalData { get; set; }
+
+        public SimulationResult()
+        {
+            AdditionalData = new Dictionary<string, object>();
+        }
+    }
+    
+}
